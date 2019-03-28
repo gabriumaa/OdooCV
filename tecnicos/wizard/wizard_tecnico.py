b05306973d889bc746eb503638b7cc9ed7253721
franciscomg <fmelendezg@ice.go.cr>
2019-03-28 16:31:04 -0600

    prueba cargar modulos

---
+++ tecnicos/wizard/wizard_tecnico.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+class WizardTecnico(models.TransientModel):
+    _name = 'tecnicos.wizard'
+    user = fields.Many2one( comodel_name="res.users",
+                          string="Usuario a asignar",
+                          required=True )
+    date = fields.Date(string="Fecha a revisar",
+                      required=False)
+
+    @api.multi
+    def register_users(self):
+        if self._context.get('active_ids',False):
+            obj_tecnicos = self.env['tecnicos.odoo'].browse(self._context.get('active_ids',False))
+            for rec in obj_tecnicos:
+                rec.check_date = self.date
+                rec.check_user = self.user.id
