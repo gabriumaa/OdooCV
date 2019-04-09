fe123a2203eb3d0f9fa531a7c2eaa699c5365e98
franciscomg <fmelendezg@ice.go.cr>
2019-04-09 16:00:04 -0600

    Primer commit de cantones y distritos

--- Cantones_Distritos_CR/__manifest__.py
+++ Cantones_Distritos_CR/__manifest__.py
@@ -1,34 +1,31 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Control de técnicos",
+    'name': "Cantones y distritos de Costa Rica",
 
     'summary': """
-        Técnicos y su control""",
+        Contiene la información de la división geografíca de Costa Rica""",
 
     'description': """
-        El proposito de este módulo es controlar a los técnicos
+        Completa la información de contacto y módulos asociados sobre los datos de dirección de Costa Rica.
     """,
 
-    'author': "Cable Visión",
+    'author': "Cable Visión de Costa Rica",
     'website': "http://www.cablevision.cr",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.2',
+    'category': 'Localization',
+    'version': '1.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'mail'],
+    'depends': ['base'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/tecnicos_view.xml',
-        'data/cron.xml',
-        'data/mail_template.xml',
-        'security/security.xml',
-        'security/ir.model.access.csv',
-        'wizard/wizard_tecnico.xml'
+        'data/cantones.xml',
+        'data/distritos.xml'
     ],
 }
--- tecnicos/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import tecnicos, titulos, vehiculos, licencias
