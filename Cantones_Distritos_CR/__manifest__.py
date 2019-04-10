7d9f06b08c4b02b6c521be6699de1cd468bc88e9
franciscomg <fmelendezg@ice.go.cr>
2019-04-10 10:50:37 -0600

    Modelos de cantones y distritos modificado

--- Cantones_Distritos_CR/__manifest__.py
+++ Cantones_Distritos_CR/__manifest__.py
@@ -24,7 +24,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        #'views/tecnicos_view.xml',
+        'views/canton_distrito.xml',
         'data/canton.xml',
         'data/distrito.xml'
     ],
