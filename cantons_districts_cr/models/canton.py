5d4ca407f577a1e1c7da81458ced6467bc46f53c
franciscomg <fmelendezg@ice.go.cr>
2019-04-22 14:28:32 -0600

    [Update] Prefinal 2 cambios en foreign key de canton y provincia

--- cantons_districts_cr/models/canton.py
+++ cantons_districts_cr/models/canton.py
@@ -7,16 +7,15 @@ class canton(models.Model):
     _name = 'res.country.state.canton'
     _description = 'Cantón'
 
-    id = fields.Char( string="Código de cantón", size=3, required=True )
+    code = fields.Char( string="Código de cantón", size=3, required=True )
     name = fields.Char( string="Nombre cantón", required=True )
     state_id = fields.Many2one( comodel_name="res.country.state", string="Provincia", required=True )
     country_id = fields.Many2one( comodel_name="res.country", string="País", required=True )
 
-
-@api.multi
-@api.onchange('country_id')
-def _onchange_country(self):
-    if self.country_id:
-        return {'domain': {'state_id': [('res.country.state.country_id', '=', self.country_id.id)]}}
-    else:
-        return {'domain': {'state_id': []}}
+    @api.multi
+    @api.onchange('country_id')
+    def _onchange_country(self):
+        if self.country_id:
+            return {'domain': {'state_id': [('res.country.state.country_id', '=', self.country_id.id)]}}
+        else:
+            return {'domain': {'state_id': []}}
