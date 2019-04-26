9b84184bcc2bfeb160e5fcbc54d74ab4eed5c3ae
franciscomg <fmelendezg@ice.go.cr>
2019-04-25 21:06:35 -0600

    [Pruebas] Listo seguridad módulo de cantones y distritos

--- cantons_districts_cr/__manifest__.py
+++ cantons_districts_cr/__manifest__.py
@@ -1,28 +1,28 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Cantones y distritos de Costa Rica",
+    "name": "Cantones y distritos de Costa Rica",
 
-    'summary': """
+    "summary": """
         División geografíca de Costa Rica""",
 
-    'description': """
+    "description": """
         Completa la información de contacto y módulos asociados sobre los datos de dirección de Costa Rica.""",
 
-    'author': "Cable Visión de Costa Rica",
-    'website': "https://www.cablevision.cr",
+    "author": "Cable Visión de Costa Rica",
+    "website": "https://www.cablevision.cr",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': "Localization",
-    'version': "1.1",
+    "category": "Localization",
+    "version": "1.1",
 
     # any module necessary for this one to work correctly
-    'depends': ["base", "crm", "contacts"],
+    "depends": ["base", "crm", "contacts"],
 
     # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
+    "data": [
+        "security/ir.model.access.csv",
         "views/canton_district.xml",
         "views/inherit_crm.xml",
         "views/inherit_contact.xml",
