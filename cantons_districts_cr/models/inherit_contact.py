e37b8be2593c277afe6a3a70f32243490f71eb5f
franciscomg <fmelendezg@ice.go.cr>
2019-04-29 13:24:45 -0600

    [Arreglo] Reducción de variables usadas, city ahora es cantón

--- cantons_districts_cr/models/inherit_contact.py
+++ cantons_districts_cr/models/inherit_contact.py
@@ -6,10 +6,12 @@ from odoo import models, fields, api
 class inheritContact(models.Model):
     _inherit = "res.partner"
 
-    district_id = fields.Many2one( comodel_name="res.country.state.canton.district", domain="[('canton_id','=',canton_id)]" )
-    canton_id = fields.Many2one( comodel_name="res.country.state.canton", domain="[('state_id','=',state_id)]" )
+    district_id = fields.Many2one( comodel_name="res.country.state.canton.district", string="Distrito", domain="[('canton_id','=',city)]" )
+    city = fields.Many2one( comodel_name="res.country.state.canton", string="Cantón", domain="[('state_id','=',state_id)]" )
 
     @api.onchange('district_id')
     def _onchange_district(self):
         if self.district_id:
             self.zip = self.district_id.code
+        else:
+            self.zip = ""
--- cantons_districts_cr/models/inherit_crm.py
+++ cantons_districts_cr/models/inherit_crm.py
@@ -6,10 +6,12 @@ from odoo import models, fields, api
 class inheritCrm(models.Model):
     _inherit = "crm.lead"
 
-    district_id = fields.Many2one( comodel_name="res.country.state.canton.district", domain="[('canton_id','=',canton_id)]" )
-    canton_id = fields.Many2one( comodel_name="res.country.state.canton", domain="[('state_id','=',state_id)]" )
+    district_id = fields.Many2one( comodel_name="res.country.state.canton.district", string="Distrito", domain="[('canton_id','=',city)]" )
+    city = fields.Many2one( comodel_name="res.country.state.canton", string="Cantón", domain="[('state_id','=',state_id)]" )
 
     @api.onchange('district_id')
     def _onchange_district(self):
         if self.district_id:
             self.zip = self.district_id.code
+        else:
+            self.zip = ""
