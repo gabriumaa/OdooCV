080131963079d9b1c323ca0475f4fdad63eb589c
franciscomg <fmelendezg@ice.go.cr>
2019-05-08 12:12:55 -0600

    [Fix] Cambio de elementos por compatibilidad versión 12

--- prospects/models/inherit_crm.py
+++ prospects/models/inherit_crm.py
@@ -6,11 +6,13 @@ from odoo import models, fields, api
 class inheritCrm(models.Model):
     _inherit = "crm.lead"
 
-    company_type = fields.Selection( [("person", "Individual"), ("company", "Compañía")], string="Tipo de cliente", default="person" )
+    company_type = fields.Selection( [("person", "Residencial"), ("company", "Empresarial")], string="Tipo de cliente", default="person" )
     vat = fields.Char( string="Cédula o identificación" )
+    function = fields.Many2one( comodel_name="res.partner.industry", string="Puesto de trabajo" )
     electric_company = fields.Selection( [("cnfl", "CNFL"), ("ice", "ICE"), ("esph", "ESPH"), ("jasec", "JASEC")], string="Compañía eléctrica" )
     nise = fields.Char( string="NISE" )
     node = fields.Char( string="Nodo" )
     plate = fields.Char( string="Placa" )
     other_phone = fields.Char( string="Otro teléfono" )
-    function = fields.Many2one( comodel_name="res.partner.industry", string="Puesto de trabajo" )
+    product_ids = fields.Many2many( comodel_name="product.product", string="Servicios de interés" )
+
