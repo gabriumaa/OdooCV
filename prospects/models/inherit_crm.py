f4cf28d23daa7f26a8a8c1cf88ed90aa4e6203b8
franciscomg <fmelendezg@ice.go.cr>
2019-05-16 14:40:09 -0600

    [Add] Cambios para aumentar la automatización del sistema

--- prospects/models/inherit_crm.py
+++ prospects/models/inherit_crm.py
@@ -7,6 +7,7 @@ class inheritCrm(models.Model):
     _inherit = "crm.lead"
 
     company_type = fields.Selection( [("person", "Residencial"), ("company", "Empresarial")], string="Tipo de cliente", default="person" )
+    planned_revenue = fields.Monetary( currency_field='company_currency', string='Expected Revenue', compute='_products_total' )
     vat = fields.Char( string="Cédula o identificación" )
     function = fields.Many2one( comodel_name="res.partner.industry", string="Puesto de trabajo" )
     electric_company = fields.Selection( [("cnfl", "CNFL"), ("ice", "ICE"), ("esph", "ESPH"), ("jasec", "JASEC")], string="Compañía eléctrica" )
@@ -16,3 +17,30 @@ class inheritCrm(models.Model):
     other_phone = fields.Char( string="Otro teléfono" )
     product_ids = fields.Many2many( comodel_name="product.product", string="Servicios de interés" )
 
+    @api.multi
+    def action_apply_lead(self):
+        """ Override de método action_apply de crm.lead2opportunity.partner
+        """
+        self.ensure_one()
+        values = {
+            'name': 'convert',
+            'team_id': self.team_id.id,
+            'action': 'nothing'
+        }
+
+        if self.partner_id:
+            values['partner_id'] = self.partner_id.id
+
+        values.update({'lead_ids': self.id, 'user_ids': [self.user_id.id]})
+        obj = self.env["crm.lead2opportunity.partner"].create(values)
+        obj._convert_opportunity(values)
+
+        return self.redirect_opportunity_view()
+
+    @api.depends('product_ids')
+    def _products_total(self):
+        total = 0
+        for product in self.product_ids:
+            total += product.lst_price
+
+        self.planned_revenue = str(total)
