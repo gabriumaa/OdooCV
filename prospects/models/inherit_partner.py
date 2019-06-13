8737f61eaabce1ea73b6e949544611e4d7e4306e
franciscomg <fmelendezg@ice.go.cr>
2019-06-12 23:41:05 -0600

    [FIX] Mejoras del módulo para minimizar código

--- prospects/models/inherit_partner.py
+++ prospects/models/inherit_partner.py
@@ -7,8 +7,8 @@ class InheritPartner(models.Model):
     _inherit = "res.partner"
 
     client_number = fields.Char(index=True, string="Número de cliente")
-    #first_name = fields.Char(string="Nombre")
-    #last_name = fields.Char(string="Apellidos")
+    first_name = fields.Char(string="Nombre")
+    last_name = fields.Char(string="Apellidos")
     company_type = fields.Selection(selection=[("person", "Residencial"), ("company", "Empresarial")], string="Company Type", compute="_compute_company_type", inverse="_write_company_type")
     function = fields.Many2one(comodel_name="res.partner.industry", string="Puesto de trabajo")
     electric_company = fields.Selection(selection=[("cnfl", "CNFL"), ("ice", "ICE"), ("esph", "ESPH"), ("jasec", "JASEC")], string="Compañía eléctrica")
